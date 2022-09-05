b1117706172c81a168453d87d13c0f37575e5e43
knikop <74211704+knikop@users.noreply.github.com>
2022-09-05 00:13:28 -0400

    App-Dev Lab2

---
+++ Application Development Desktop/Lab2/NikopoulosKostaLab2/LINQWithArrayOfObjects/LINQWithArrayOfObjects/Employees.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQWithArrayOfObjects
+{
+    class Employee
+    {
+        public string FirstName { get; } // read-only auto-implemented property
+        public string LastName { get; } // read-only auto-implemented property
+        private decimal monthlySalary; // monthly salary of employee
+
+        // constructor initializes first name, last name and monthly salary
+        public Employee(string firstName, string lastName,
+        decimal monthlySalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MonthlySalary = monthlySalary;
+        }
+
+        // property that gets and sets the employee's monthly salary
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value >= 0M) // validate that salary is nonnegative
+                {
+                    monthlySalary = value;
+                }
+            }
+        }
+
+        // return a string containing the employee's information
+        public override string ToString() =>
+        $"{FirstName,-10} {LastName,-10} {MonthlySalary,10:C}";
+    }
+}
---
+++ Application Development Desktop/Lab2/NikopoulosKostaLab2/LINQWithListCollection/LINQWithListCollection/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQWithListCollection
+{
+    class Program
+    {
+        static void Main(){
+             // populate a List of strings 
+            var items = new List<string>();
+                items.Add("aQua"); // add "aQua" to the end of the List
+                items.Add("RusT"); // add "RusT" to the end of the List
+                items.Add("yElLow"); // add "yElLow" to the end of the List
+                items.Add("rEd"); // add "rEd" to the end of the List
+
+            // display initial List
+            Console.Write("items contains:");
+            foreach (var item in items)
+            {
+               Console.Write($" {item}");
+            }
+
+            Console.WriteLine(); // output end of line
+
+             // convert to uppercase, select those starting with "R" and sort
+            var startsWithR =
+               from item in items
+               let uppercaseString = item.ToUpper()
+               where uppercaseString.StartsWith("R")
+               orderby uppercaseString
+               select uppercaseString;
+
+             // display query results
+            Console.Write("results of query startsWithR:");
+            foreach(var item in startsWithR)
+            {
+               Console.Write($" {item}");
+            }
+
+            Console.WriteLine(); // output end of line
+
+            items.Add("rUbY");
+            items.Add("SaFfRon");
+
+            // display initial List
+            Console.Write("items contains:");
+            foreach (var item in items)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine(); // output end of line
+
+             // display updated query results
+            Console.Write("results of query startsWithR:");
+            foreach (var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine(); // output end of line
+        }
+    }
+}
